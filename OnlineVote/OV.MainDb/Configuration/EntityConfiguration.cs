ac395ead7dd53fb0948daed2b84adc706e53b9b2
Vitaliy <vitaliygbbay@gmail,com>
2021-04-16 01:01:12 +0200

    DEV-1 Add EF, OvMainDbContext file and first persisted model

---
+++ OnlineVote/OV.MainDb/Configuration/EntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OV.MainDb.Configuration
+{
+    public abstract class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class
+    {
+        public abstract void Configure(EntityTypeBuilder<TEntity> builder);
+    }
+}
---
+++ OnlineVote/OV.MainDb/Province/Models/PersistedProvince.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OV.MainDb.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OV.MainDb.Province.Models
+{
+    [Table("tblProvinces")]
+    public class PersistedProvince
+    {
+        [Column("Id")] public int Id { get; set; }
+        [Column("Name")] public string Name { get; set; }
+
+    }
+
+    public class ProvinceConfiguration : EntityConfiguration<PersistedProvince>
+    {
+        public override void Configure(EntityTypeBuilder<PersistedProvince> builder)
+        {
+            builder.HasKey(p => p.Id);
+        }
+    }
+}
